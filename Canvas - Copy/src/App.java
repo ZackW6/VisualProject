b8f3616200f77a79afa7dc9e6ba17d539ccea48d
Zack White <zw1045072@francistuttle.edu>
2024-02-15 12:26:48 -0600

    Stuff

--- Canvas - Copy/src/App.java
+++ Canvas - Copy/src/App.java
@@ -1,9 +1,25 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class App {
     public static void main(String[] args) {
-        VisualJava vis = new VisualJava();
+        VisualJ vis = new VisualJ("Simulation",900,900,Color.black);
         vis.startGraphics();
 
         // VisualJava vis2 = new VisualJava();
         // vis2.startGraphics();
+        Thread thread = new Thread(() -> {
+            while (true) {
+                sleep();
+                runAll();
+            }
+        });
+        thread.start();
+    }
+    public static void runAll(){
+        System.out.println("HI");
     }
 }
--- Canvas - Copy/src/VisualJ.java
+++ Canvas - Copy/src/VisualJ.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
-abstract class VisualJ extends JFrame{
+class VisualJ extends JFrame{
     public Obj[] shapes=new Obj[100];
     //shapes encompasses everything that will be included in the graphics project
     int WIDTH = 20000;
@@ -105,5 +107,65 @@ abstract class VisualJ extends JFrame{
             }
         });
         animationThread.start(); // Start the animation thread
+    }
+
+
+
+
+    public VisualJ(String title,int width, int height, Color background){
+        WIDTH=width;
+        HEIGHT=height;
+        SwingUtilities.invokeLater(() -> setVisible(true));
+        setArrSize(10001);
+        Timer timer = new Timer(1, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                runAll();
+            }
+        });
+        timer.start();
+
+
+        // Perform the operation you want to measure
+
+
+
+
+        // Stop the timer
+        setTitle(title);
+        setSize(width, height);
+        setBackground(background);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        for (int i=0;i<shapes.length-1;i++){
+            int rand1=Random.randInt(0, WIDTH);
+            int radn2=Random.randInt(0,HEIGHT);
+            int dist=(int)((Math.sqrt((Math.pow(WIDTH/2-rand1,2))+(Math.pow(HEIGHT/2-radn2,2))))/50);
+            shapes[i]=new Square(rand1,radn2,1+dist,ColorEXT.getRandomColor(),true);
+            //shapes[i]=new Circle(rand1,radn2,1+dist,ColorEXT.getRandomColor(),true);
+            //shapes[i]=new Polygon(rand1,radn2,new int[]{0,2+dist,(int)(1+dist/2)},new int[]{0,0,3+dist},ColorEXT.getRandomColor(), false);
+            //shapes[i]=new Line(rand1,radn2,new int[]{0,2+dist},new int[]{0,3+dist},ColorEXT.getColorBasedXY(rand1,radn2,WIDTH,HEIGHT), 1+dist);
+            //shapes[i]=new Text(rand1,radn2,1+dist*10,ColorEXT.getRandomColor(),"HI");//TEXT BROKEN
+            //shapes[i].customRotationCenter(WIDTH/2,HEIGHT/2);
+        }
+        // shapes[9999]=new Circle(100,100,100,ColorEXT.getRandomColor(),true);
+        // Text txt=new Text(100, 100, 200, Color.blue, 20.0+"");
+        // shapes[10000]=txt;
+
+
+    }
+    private void runAll(){
+        for (int i=0;i<shapes.length;i++){
+            if (shapes[i]!=null){
+                int rand=Random.randInt(0, 5);
+                shapes[i].rotate(shapes[i].degrees2+rand);
+                shapes[i].rotPoint(WIDTH/2,HEIGHT/2,shapes[i].degrees2+rand);
+               
+                //shapes[i].setPosition(Random.randInt(0,WIDTH),Random.randInt(0,HEIGHT));
+                //shapes[i].setColor(ColorEXT.getColorBasedXY(shapes[i].xcoord, shapes[i].ycoord, WIDTH, HEIGHT));
+               
+            }
+        }
+        //repaint(0,0,WIDTH,HEIGHT);
     }
 }
