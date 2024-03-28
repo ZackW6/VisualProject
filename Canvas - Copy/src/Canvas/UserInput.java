9f8b7ee17d9c3c710bf7511a0113d8efeaca363b
Zack White <zw1045072@francistuttle.edu>
2024-03-28 15:33:48 -0500

    stuff

--- Canvas - Copy/src/Canvas/UserInput.java
+++ Canvas - Copy/src/Canvas/UserInput.java
@@ -5,31 +5,10 @@ import java.awt.event.MouseWheelEvent;
 import java.util.Scanner;
 
 import javax.swing.SwingUtilities;
-
-public class UserInput {
-    private double[] mouseCoords = new double[2];
-
-    private static boolean isMousePressed;
-    private double[] lastMouseClickedCoords = new double[2];
-
-    private double[] lastMousePressedCoords = new double[2];
-    private double whenLastMousePressed;
-
-    private double[] lastMouseReleasedCoords = new double[2];
-    private double whenLastMouseReleased;
-
-    private boolean mouseIsEntered = false;
-    private double[] lastMouseExitedCoords = new double[2];
-    private double whenLastMouseExited;
-
-    private double[] lastMouseEnteredCoords = new double[2];
-    private double whenLastMouseEntered;
-
-    private boolean isMouseDragging = false;
-    private double[] mouseDragCurrentCoords = new double[2];
-    private double whenLastMouseDrag;
-
-    private VisualJ canvas;
+/**
+ * Class for all user input actions
+ */
+public class UserInput {    
     public static String getNextLine() {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
@@ -52,55 +31,96 @@ public class UserInput {
     }
 
     public void setupMouseInputs(VisualJ canvas){
+        
+    }
+}
+
+
+class MouseInputs{
+
+    private VisualJ canvas;
+
+    public int[] mouseCoords = new int[2];
+    public double mouseWheel = 0;
+
+    public MouseData mouseEntered;
+
+    public MouseData mouseRightPressed;
+    public MouseData mouseRightClicked;
+    public MouseData mouseRightDragging;
+
+    public MouseData mouseLeftPressed;
+    public MouseData mouseLeftClicked;
+    public MouseData mouseLeftDragging;
+
+
+    public MouseInputs(VisualJ canvas){
+        this.canvas = canvas;
         canvas.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 if (SwingUtilities.isLeftMouseButton(e)) {
-                    
+                    mouseLeftPressed.setCoords(new int[]{e.getX(),e.getY()});
+                    mouseLeftPressed.setIsTrue(true);
                 }
                 if (SwingUtilities.isRightMouseButton(e)) {
-                    
+                    mouseRightPressed.setCoords(new int[]{e.getX(),e.getY()});
+                    mouseRightPressed.setIsTrue(true);
                 }
             }
             @Override
             public void mouseReleased(MouseEvent e) {
                 if (SwingUtilities.isLeftMouseButton(e)) {
-                    
+                    mouseLeftPressed.setSecondCoords(new int[]{e.getX(),e.getY()});
+                    mouseLeftPressed.setIsTrue(false);
+                    mouseLeftClicked.setSecondCoords(new int[]{e.getX(),e.getY()});
+                    mouseLeftClicked.setIsTrue(false);
+                    mouseLeftDragging.setSecondCoords(new int[]{e.getX(),e.getY()});
+                    mouseLeftDragging.setIsTrue(false);
                 }
                 if (SwingUtilities.isRightMouseButton(e)) {
-                    
+                    mouseRightPressed.setSecondCoords(new int[]{e.getX(),e.getY()});
+                    mouseRightPressed.setIsTrue(false);
+                    mouseRightClicked.setSecondCoords(new int[]{e.getX(),e.getY()});
+                    mouseRightClicked.setIsTrue(false);
+                    mouseRightDragging.setSecondCoords(new int[]{e.getX(),e.getY()});
+                    mouseRightDragging.setIsTrue(false);
                 }
             }
             @Override
             public void mouseClicked(MouseEvent e) {
                 if (SwingUtilities.isLeftMouseButton(e)) {
-                    
+                    mouseLeftClicked.setCoords(new int[]{e.getX(),e.getY()});
+                    mouseLeftClicked.setIsTrue(true);
                 }
                 if (SwingUtilities.isRightMouseButton(e)) {
-                    
+                    mouseRightPressed.setCoords(new int[]{e.getX(),e.getY()});
+                    mouseRightPressed.setIsTrue(true);
                 }
             }
             @Override
             public void mouseEntered(MouseEvent e) {
-                lastMouseEnteredCoords = new double[]{e.getX(), e.getY()};
-                whenLastMouseEntered = canvas.getTimeStep();
+                mouseEntered.setCoords(new int[]{e.getX(),e.getY()});
+                mouseEntered.setIsTrue(true);
             }
             @Override
             public void mouseExited(MouseEvent e) {
-                lastMouseExitedCoords = new double[]{e.getX(), e.getY()};
-                whenLastMouseExited = canvas.getTimeStep();
+                mouseEntered.setSecondCoords(new int[]{e.getX(),e.getY()});
+                mouseEntered.setIsTrue(false);
             }
             @Override
             public void mouseWheelMoved(MouseWheelEvent e){
-                // e.();
+                mouseWheel=e.getPreciseWheelRotation();
             }
             @Override
             public void mouseDragged(MouseEvent e){
                 if (SwingUtilities.isLeftMouseButton(e)) {
-                    
+                    mouseLeftDragging.setCoords(new int[]{e.getX(),e.getY()});
+                    mouseLeftDragging.setIsTrue(true);
                 }
                 if (SwingUtilities.isRightMouseButton(e)) {
-                    
+                    mouseRightDragging.setCoords(new int[]{e.getX(),e.getY()});
+                    mouseRightDragging.setIsTrue(true);
                 }
             }
             @Override
@@ -112,13 +132,70 @@ public class UserInput {
         this.canvas=canvas;
     }
 
-    public double getMouseX() {
-        return mouseCoords[0];
+    public int[] getMouseCoords() {
+        return mouseCoords;
     }
 
-    public double getMouseY() {
-        return mouseCoords[1];
+    public MouseData getEntered(){
+        return mouseEntered;
     }
 
-    
+    public MouseData getLeftPressed(){
+        return mouseLeftPressed;
+    }
+
+    public MouseData getLeftClicked(){
+        return mouseLeftClicked;
+    }
+
+    public MouseData getLeftDragging(){
+        return mouseLeftDragging;
+    }
+
+    public MouseData getRightPressed(){
+        return mouseRightPressed;
+    }
+
+    public MouseData getRightClicked(){
+        return mouseRightClicked;
+    }
+
+    public MouseData getRightDragging(){
+        return mouseRightDragging;
+    }
+
+    public double getMouseWheelPosition(){
+        return mouseWheel;
+    }
+}
+/**
+ * Contains generalized data about where a last 
+ * mouse event occured contained in Coords,
+ * then where the event ended in SecondCoords,
+ * finally if the event is happening or not in IsTrue.
+ * 
+ * @see UserInput
+ */
+class MouseData{
+    private int[] coords = {-1,-1};
+    private int[] coords2 = {-1,-1};
+    private boolean isTrue = false;
+    public void setCoords(int[] coords){
+        this.coords = coords;
+    }
+    public void setSecondCoords(int[] coords){
+        this.coords2 = coords;
+    }
+    public void setIsTrue(boolean isTrue){
+        this.isTrue = isTrue;
+    }
+    public int[] getCoords(){
+        return coords;
+    }
+    public int[] getSecondCoords(){
+        return coords2;
+    }
+    public boolean getIsTrue(){
+        return isTrue;
+    }
 }
